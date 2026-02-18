96b565cfb22c829caf24eb8e98c50bf4b135167b
Bautista Heyde <bautistaheyde@hotmail.com>
2026-02-18 12:58:26 -0300

    fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { ProfilesController } from './profiles.controller';
@@ -6,6 +6,7 @@ import { PublicMembersController } from './public-members.controller';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController, ProfilesController, PublicMembersController],
